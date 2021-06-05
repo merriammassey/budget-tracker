23ec4cb81dca8b86f3b83f1d190b098ba29f1ca6
Maria M <prov21_1@hotmail.com>
2021-06-05 08:44:41 -0700

    cache management

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,4 @@
-const FILES_TO_CACHE = [
-  "./index.html",
-  "./events.html",
-  "./tickets.html",
-  "./schedule.html",
-  "./assets/css/style.css",
-  "./assets/css/bootstrap.css",
-  "./assets/css/tickets.css",
-  "./dist/app.bundle.js",
-  "./dist/events.bundle.js",
-  "./dist/tickets.bundle.js",
-  "./dist/schedule.bundle.js",
-];
+const FILES_TO_CACHE = ["./index.html", "./public/css/style.css"];
 const APP_PREFIX = "FoodFest-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
@@ -20,6 +8,30 @@ self.addEventListener("install", function (e) {
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+//tell service worker how to manage caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    //return array of cache names or keyList
+    caches.keys().then(function (keyList) {
+      //filter out cache names with app prefix and save them in array called cacheKeepList
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      //add current cache to keep list
+      cacheKeeplist.push(CACHE_NAME);
+      //return promise that resolves when old caches have been delieted
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
     })
   );
 });
