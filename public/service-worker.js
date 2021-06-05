b883bc7b460154df7ff4787b134e38a0e3bd2057
Maria M <prov21_1@hotmail.com>
2021-06-05 09:15:58 -0700

    retrieve resources from cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,13 @@
 const FILES_TO_CACHE = ["./index.html", "./public/css/style.css"];
-const APP_PREFIX = "FoodFest-";
+const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
+//listen for install event, capture event with e
 self.addEventListener("install", function (e) {
+  //tell browser to wait until the following code is executed
   e.waitUntil(
+    //open cache storage instance
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
@@ -25,13 +28,38 @@ self.addEventListener("activate", function (e) {
       cacheKeeplist.push(CACHE_NAME);
       //return promise that resolves when old caches have been delieted
       return Promise.all(
+        //look through key list
         keyList.map(function (key, i) {
+          //if key isn't in cache keep list, delete it from cache
+          //only return value of -1 if value isn't found in key list
           if (cacheKeeplist.indexOf(key) === -1) {
             console.log("deleting cache : " + keyList[i]);
             return caches.delete(keyList[i]);
           }
         })
       );
+    })
+  );
+});
+
+//retrieve information from cache
+self.addEventListener("fetch", function (e) {
+  //log URL of requested resource
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    //check to see if the requested resource matches any resource in the cache
+    caches.match(e.request).then(function (request) {
+      //if it is in the cache, respond with cached resource
+      if (request) {
+        console.log("responding with cache : " + e.request.url);
+        return request;
+        //if it's not in the cache, make a normal request
+      } else {
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+      // can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   );
 });
